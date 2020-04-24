5193364e361d1b90862c8ee0f3e372bbd73258eb
Eugene <enot8826@gmail.com>
2020-04-24 11:36:54 -0700

    FAQ page

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="faq">
-
+        <v-container>
+            <v-row class="text-center">
+                <v-col cols="12">
+                    <h1 :style="{'color': theme_style.navigation.panel.title.color}">FAQ</h1>
+                </v-col>
+            </v-row>
+            <v-row class="text-center">
+                <v-col cols="12">
+                    <h2 :style="{'color': theme_style.navigation.panel.subtitle.color}" class="font-italic">Coming Soon!</h2>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script>
     export default {
-        name: "FAQ"
+        name: "FAQ",
+        props: ['theme_style']
     }
 </script>
 
